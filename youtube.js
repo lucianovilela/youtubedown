d6d61ce9f15fd6107c618a0cf65c16eed7cc0a23
lucianovilela <lucianovilela@gmail.com>
2023-12-06 19:26:00 +0000

    wip

--- youtube.js
+++ youtube.js
@@ -6,23 +6,24 @@ let url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
 const getInfo = (req, res, next) => {
     ytdl.getInfo(req.query.url)
-    .then(info=>{
-        res.json(info);
-    })
-    .catch(err=>res.send(err.message))
-   
+        .then(info => {
+            res.json(info);
+        })
+        .catch(err => res.send(err.message))
+
 }
 
 const download = (req, res, next) => {
     let url = req.query.url;
-    let format = req.query.format;
+
     ytdl.getInfo(url)
-    .then(info=>{
-        let cformat = ytdl.chooseFormat(info.formats, {itag:format})
-        res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${cformat.container}"`)
-        ytdl(url, cformat).pipe(res)
-    })
-    .catch(err=>res.send(err.message))
+        .then(info => {
+            let format = ytdl.chooseFormat(info.formats, {quality:req.query.format})
+            console.log(format)
+            res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${format.container}"`)
+            ytdl(url, { begin: req.query.start, format: format }).pipe(res)
+        })
+        .catch(err => res.send(err.message))
 }
 
-module.exports = {getInfo, download}
+module.exports = { getInfo, download }
